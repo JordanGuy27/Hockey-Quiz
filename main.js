169fbbd3652312e696a31e6e3c10544038281a11
Jordan Guy <jordan.guy.277@gmail.com>
2018-02-08 18:03:48 -0500

    getting there

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const playersArray = ["Auston Matthews", "Morgan Rielly", "Frederik Andersen", "Mitch Marner", "William Nylander"];
+const playersArray = ["Auston Matthews", "Morgan Rielly", "Mitch Marner", "William Nylander", "Nikita Zaitsev"];
+const goaliesArray = ["Frederik Andersen", "Curtis McElhinney"];
 
-const myApp = {};
 
-$(function(){
-
+$(function() {
     $('form').on('submit', function(event) {
 
+
         event.preventDefault();
+
+
+        let horn = new Audio('assets/toronto maple leafs.wav'); 
+        horn.play();
 
         let question1 = $('input[name=q1]:checked').val();
         let question2 = $('input[name=q2]:checked').val();
@@ -17,7 +21,7 @@ $(function(){
         const questionArray = [];
        
         questionArray.push(question1, question2, question3, question4, question5);
-        // console.log(questionArray);
+        
 
         let scoreArray = questionArray.map(function(item){
             return parseInt(item);
@@ -28,9 +32,37 @@ $(function(){
         
         console.log(scoreArray);
 
-        if (scoreArray === 65) {
+
+        if (scoreArray >= 900) {
+            $('.results').html(`<h3>Game misconduct for not liking Hockey</h3>`);
+        } else if (scoreArray === 65) {
+            $('.results').html(`<h3>${playersArray[4]}</h3>`);
+        } else if (scoreArray === 56) {
             $('.results').html(`<h3>${playersArray[1]}</h3>`);
+        } else if (scoreArray === 44) {
+            $('.results').html(`<h3>${playersArray[2]}</h3>`);
+        } else if (scoreArray === 55) {
+            $('.results').html(`<h3>${playersArray[0]}</h3>`);
+        } else if (scoreArray === 38) {
+            $('.results').html(`<h3>${playersArray[3]}</h3>`);
+        } else {
+           $('.results').html(`<h3>Might need to try another team bud</h3>`);
         }
+
+
+        function randomGoalie(array) {
+            const randomArray = Math.floor(Math.random() * array.length);
+            return array[randomArray];
+        }
+
+        const goalieChoice = randomGoalie(goaliesArray);
+     
+        if (scoreArray === 57) {
+            $('.results').html(`<h3>${goalieChoice}</h3>`);
+        }
+
+        $('input[name=reset]').on('click', function () {
+            $('.results').html(`<h3></h3>`)
+        });
     });
 });
-
